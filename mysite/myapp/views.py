77e2fe91c137a7ab28a7acf0d7639e336eadca35
Victor Ambrose <victorambrose11@gmail.com>
2025-02-02 23:33:05 +0530

    Testing OP rendering

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,14 +1,21 @@
 # myapp/views.py
 from django.shortcuts import render
 from .forms import TextForm
+from textblob import TextBlob
+
+def rephrase_text(text):
+    blob = TextBlob(text)
+    return blob.correct()  
 
 def home(request):
+    text = ""
     if request.method == 'POST':
         form = TextForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['text']
-            return render(request, 'myapp/home.html', {'form': form, 'text': text})
+            rephrased_text = str(rephrase_text(text))
+            return render(request, 'myapp/home.html', {'form': form, 'text': rephrased_text})
     else:
         form = TextForm()
 
-    return render(request, 'myapp/home.html', {'form': form})
+    return render(request, 'myapp/home.html', {'form': form, 'text': text})
